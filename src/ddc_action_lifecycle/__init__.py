154a7377529391cf3bee744dce017f01afa55759
Altru.dev <altrudevelop@gmail.com>
2026-09-20 21:24:40 -0700

    lifecycle: expose assurance and persistence APIs

--- src/ddc_action_lifecycle/__init__.py
+++ src/ddc_action_lifecycle/__init__.py
@@ -1,19 +1,34 @@
 """DDC Action Lifecycle reference implementation."""
 
+from .assurance import (
+    DecisionAssessment,
+    assess_decision,
+    next_transition_admissibility,
+)
+from .io import dumps_jsonl, load_jsonl, loads_jsonl, save_jsonl
 from .ledger import (
     LifecycleEvent,
     LifecycleLedger,
     LifecycleValidationError,
     create_event,
     event_digest,
+    event_from_dict,
 )
 
 __all__ = [
+    "DecisionAssessment",
     "LifecycleEvent",
     "LifecycleLedger",
     "LifecycleValidationError",
+    "assess_decision",
     "create_event",
+    "dumps_jsonl",
     "event_digest",
+    "event_from_dict",
+    "load_jsonl",
+    "loads_jsonl",
+    "next_transition_admissibility",
+    "save_jsonl",
 ]
 
 __version__ = "0.1.0"
